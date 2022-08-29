76fc6afdb35dfcc3d60b69aa0ee724fa56ea218e
Hans Yelek <hnyelek@gmail.com>
2022-08-29 13:16:38 -0600

    Add component: ModelDetail404

---
+++ src/home/ModelDetail404.js
@@ -0,0 +1,16 @@
+import PT from 'prop-types';
+
+function ModelDetail404({ modelId }) {
+  return (
+    <div>
+      <p>
+        Model with name "{modelId}" does not exist. 
+      </p>   
+      <p>
+      TODO: link to home
+      </p>
+    </div>
+  );
+}
+
+export default ModelDetail404;
