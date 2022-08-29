1deab680d1ab34915a512092fb918b3aec9868e0
Hans Yelek <hnyelek@gmail.com>
2022-08-29 13:15:38 -0600

    Add component: HomePage

---
+++ src/home/HomePage.js
@@ -0,0 +1,13 @@
+import HeroSection from './HeroSection';
+import Gallery from './Gallery';
+
+function HomePage() {
+  return (
+    <div>
+      <HeroSection />
+      <Gallery />
+    </div>
+  );
+}
+
+export default HomePage;
