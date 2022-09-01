6da7d35acedeb42884b35727954148fb809f9388
Hans Yelek <hnyelek@gmail.com>
2022-08-31 20:28:21 -0600

    Update ModelDetail: Replace placeholder text with 3d model

--- src/home/ModelDetail.js
+++ src/home/ModelDetail.js
@@ -1,14 +1,21 @@
 import PT from 'prop-types';
 import { useParams } from 'react-router-dom';
+import ModelScene from '../3d/ModelScene';
 import styles from './ModelDetail.module.css';
 
 function ModelDetail(props) {
   return (
     <div className={styles['model-detail']}>
-    
-    <h3>
-      model detail for "{props.model.name}"
-      </h3>
+      <ModelScene model={props.model} />
+
+      <section className={styles['model-detail-copy']}>
+        <h2>
+          {props.model.name}
+        </h2>
+        <h3>
+          {props.model.category} 
+        </h3>
+      </section> 
     </div>
   );
 }
