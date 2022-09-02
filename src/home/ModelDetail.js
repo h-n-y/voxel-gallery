8023086c01ce222f0a1ec76d4830e8472b9a57c7
Hans Yelek <hnyelek@gmail.com>
2022-09-02 17:13:11 -0600

    Update ModelDetail: Add a dismiss button

--- src/home/ModelDetail.js
+++ src/home/ModelDetail.js
@@ -1,13 +1,16 @@
 import PT from 'prop-types';
 import { useParams } from 'react-router-dom';
 import ModelScene from '../3d/ModelScene';
+import DismissButton from '../common/DismissButton';
 import styles from './ModelDetail.module.css';
 
 function ModelDetail(props) {
   return (
     <div className={styles['model-detail']}>
+
       <ModelScene model={props.model} />
 
+    {/* TODO: extract into a <ModelDescription /> component */}
       <section className={styles['model-detail-copy']}>
         <h2>
           {props.model.name}
@@ -16,6 +19,10 @@ function ModelDetail(props) {
           {props.model.category} 
         </h3>
       </section> 
+
+      <div className={styles['dismiss-button-box']}>
+        <DismissButton />
+      </div>
     </div>
   );
 }
