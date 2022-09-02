4907ab3077fc7a589a75a5f0a45896abe164d2d6
Hans Yelek <hnyelek@gmail.com>
2022-09-02 17:54:14 -0600

    Update ModelDetailEmptyState: DRY up copy
    Text replaced with <HeroSection />

--- src/home/ModelDetailEmptyState.js
+++ src/home/ModelDetailEmptyState.js
@@ -1,17 +1,13 @@
+import HeroSection from './HeroSection';
+
 import styles from './ModelDetailEmptyState.module.css';
 
 function ModelDetailEmptyState() {
+  const heroSectionCTA = 'Click or tap a model on the left to see it rendered in 3D!';
+
   return (
     <section className={styles['model-detail-empty-state']}>
-      <h1>
-      Voxel Model Gallery
-      </h1>
-    <p>
-    Welcome to my gallery of simple models I created with <a href="https://ephtracy.github.io/" target="_blank">MagicaVoxel</a>.
-    </p>
-    <p>
-    Click or tap a model on the left to see it rendered in 3D!
-    </p>
+      <HeroSection cta={heroSectionCTA} />
     </section>
   );
 }
