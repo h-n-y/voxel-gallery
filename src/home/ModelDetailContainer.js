f44ed5d8d1499f12636fa360e6f3b2bd4dbfae98
Hans Yelek <hnyelek@gmail.com>
2022-09-02 17:17:35 -0600

    Update ModelDetailContainer: Disable page scrolling on mobile devices
    ModelDetailContainer is displayed modally on mobile devices atop a
    scrollable gallery. This commit prevents the background gallery from
    scrolling inadvertently while user is interacting with the modal canvas.

--- src/home/ModelDetailContainer.js
+++ src/home/ModelDetailContainer.js
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchVoxelModelWithId } from '../api/api-utils';
 import ModelDetail from './ModelDetail';
 import ModelDetail404 from './ModelDetail404';
 
+const cssClassNoScroll = 'no-scroll-mobile';
+
 function ModelDetailContainer() {
   let modelId = useParams().modelId;
   let model = fetchVoxelModelWithId(modelId);
+
+  useEffect(() => {
+    document.body.classList.add(cssClassNoScroll);
+    return () => {
+      document.body.classList.remove(cssClassNoScroll);
+    };
+  }, []);
 
   return model ?
     <ModelDetail model={model} /> :
