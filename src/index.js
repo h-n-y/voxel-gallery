aa2a2da02b7e456f3de1e394d09b657549a30e73
Hans Yelek <hnyelek@gmail.com>
2022-09-02 22:43:45 -0600

    Add /styles directory for global CSS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './variables.css';
-import './reset.css';
-import './global.css';
+import './styles/variables.css';
+import './styles/reset.css';
+import './styles/global.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
